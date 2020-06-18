8b4ea57a261451de91eea606f0943d81e8e5f53e
bangquan.qian <chainz@foxmail.com>
2020-06-18 10:58:24 +0800

    commit

---
+++ src/test/java/cn/by1e/h2o/study/MyTest.java
@@ -0,0 +1,17 @@
+package cn.by1e.h2o.study;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author bangquan.qian
+ * @date 2020-06-18 10:41
+ */
+@Slf4j
+public class MyTest {
+
+    @Test
+    public void test() {
+        log.info("hello: {}", "jack");
+    }
+}
