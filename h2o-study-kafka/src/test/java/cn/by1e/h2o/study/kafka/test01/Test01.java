c2d2aaeecb820e109493199c322f4d33d52ce121
bangquan.qian <chainz@foxmail.com>
2020-07-16 11:26:47 +0800

    Update Test01.java

--- h2o-study-kafka/src/test/java/cn/by1e/h2o/study/kafka/test01/Test01.java
+++ h2o-study-kafka/src/test/java/cn/by1e/h2o/study/kafka/test01/Test01.java
@@ -1,10 +1,14 @@
 package cn.by1e.h2o.study.kafka.test01;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author bangquan.qian
  * @date 2020-06-18 11:12
  */
+@Slf4j
 public class Test01 {
 
-    
+
+
 }
