d061440b1a9152058eb55806d8e9f489398b34ef
bangquan.qian <chainz@foxmail.com>
2020-07-24 16:58:16 +0800

    Update Algorithm016.java

--- h2o-study-leetcode/src/main/java/cn/by1e/h2o/study/leetcode/algorithm/Algorithm016.java
+++ h2o-study-leetcode/src/main/java/cn/by1e/h2o/study/leetcode/algorithm/Algorithm016.java
@@ -4,6 +4,8 @@ import cn.by1e.h2o.study.leetcode.algorithm.support.Algorithm;
 import cn.by1e.h2o.study.leetcode.algorithm.support.AlgorithmFunction;
 import cn.by1e.h2o.study.leetcode.algorithm.support.AlgorithmInput;
 
+import java.util.Arrays;
+
 /**
  * 标题：最接近的三数之和
  * <p>
@@ -42,8 +44,52 @@ public class Algorithm016 implements Algorithm<Integer> {
         return sum;
     }
 
+    /**
+     * 执行用时：5ms,在所有Java提交中击败了96.06%的用户
+     * 内存消耗：39.6MB,在所有Java提交中击败了6.82%的用户
+     */
     public int threeSumClosest2(int[] nums, int target) {
-        return 0;
+        // 先排序
+        Arrays.sort(nums);
+        // 双指针优化O(n^2)
+        int sum = 0;
+        int dst = -1;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                // 第一个元素去重优化
+                continue;
+            }
+            // 双指针
+            int j = i + 1;
+            int k = len - 1;
+            while (j < k) {
+                int s = nums[i] + nums[j] + nums[k];
+                int d = s - target;
+                if (d == 0) {
+                    return s;
+                }
+                int x = Math.abs(d);
+                if (dst > x || dst == -1) {
+                    dst = x;
+                    sum = s;
+                }
+                if (d > 0) {
+                    // 移动右指针
+                    k--;
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                } else {
+                    // 移动左指针
+                    j++;
+                    while (j < k && nums[j - 1] == nums[j]) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return sum;
     }
 
     @Override
