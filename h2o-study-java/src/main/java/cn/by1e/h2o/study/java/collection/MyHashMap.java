3dcfc386d1e5a00400b10b80cd95ce79b29077cd
bangquan.qian <chainz@foxmail.com>
2020-07-30 15:34:17 +0800

    commit

--- h2o-study-java/src/main/java/cn/by1e/h2o/study/java/collection/MyHashMap.java
+++ h2o-study-java/src/main/java/cn/by1e/h2o/study/java/collection/MyHashMap.java
@@ -1,72 +1,97 @@
 package cn.by1e.h2o.study.java.collection;
 
-import java.util.Collection;
+import cn.by1e.ox.core.constant.Constants;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author bangquan.qian
  * @date 2020-07-28 11:18
  */
-public class MyHashMap<K,V> implements Map<K,V> {
+public class MyHashMap<K, V> extends HashMap<K, V> {
+    private static final long serialVersionUID = 6993500316817662150L;
 
-    @Override
-    public int size() {
-        return 0;
+    public MyHashMap(int initialCapacity, float loadFactor) {
+        super(initialCapacity, loadFactor);
     }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
+    public MyHashMap(int initialCapacity) {
+        super(initialCapacity);
     }
 
-    @Override
-    public boolean containsKey(Object key) {
-        return false;
+    public MyHashMap() {
+
     }
 
-    @Override
-    public boolean containsValue(Object value) {
-        return false;
-    }
-
-    @Override
-    public V get(Object key) {
-        return null;
+    public MyHashMap(Map<? extends K, ? extends V> m) {
+        super(m);
     }
 
     @Override
     public V put(K key, V value) {
+        Map<String, Object> hook1 = hook();
+        V v = super.put(key, value);
+        Map<String, Object> hook2 = hook();
+        return v;
+    }
+
+    private Map<String, Object> hook() {
+        try {
+            return doHook();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    @Override
-    public V remove(Object key) {
-        return null;
+    private Map<String, Object> doHook() throws Throwable {
+        Class<MyHashMap> myHashMapClass = MyHashMap.class;
+        Class<? super MyHashMap> superclass = myHashMapClass.getSuperclass();
+        Field[] fields = superclass.getDeclaredFields();
+        Map<String, Object> fieldMapValue = new HashMap<>(fields.length);
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object value = field.get(this);
+            fieldMapValue.put(field.getName(), value);
+        }
+        furtherHook(fieldMapValue);
+        return fieldMapValue;
+        //ConsoleUtils.sout("hook");
     }
 
-    @Override
-    public void putAll(Map<? extends K, ? extends V> m) {
-
+    private void furtherHook(Map<String, Object> fieldMapValue) throws Throwable {
+        furtherHook1(fieldMapValue);
     }
 
-    @Override
-    public void clear() {
+    @SuppressWarnings(Constants.UNCHECKED)
+    private void furtherHook1(Map<String, Object> fieldMapValue) throws Throwable {
+        Object object = fieldMapValue.get("table");
+        Object[] table = (Object[]) object;
 
+        List<Map<String, Object>> nodes = new ArrayList<>();
+
+        if (table != null) {
+            for (Object node : table) {
+                Map<String, Object> fieldMapValue0 = new HashMap<>();
+                if (node != null) {
+                    Class<?> aClass = node.getClass();
+                    Field[] fields = aClass.getDeclaredFields();
+                    for (Field field : fields) {
+                        field.setAccessible(true);
+                        Object value = field.get(node);
+                        fieldMapValue0.put(field.getName(), value);
+                    }
+                    fieldMapValue0.put("_class", node.getClass().getCanonicalName());
+                }
+                nodes.add(fieldMapValue0);
+            }
+        }
+
+        fieldMapValue.put("hook_table", nodes);
     }
 
-    @Override
-    public Set<K> keySet() {
-        return null;
-    }
-
-    @Override
-    public Collection<V> values() {
-        return null;
-    }
-
-    @Override
-    public Set<Entry<K, V>> entrySet() {
-        return null;
-    }
 }
--- h2o-study-java/src/test/java/cn/by1e/h2o/study/java/test/MyTest01.java
+++ h2o-study-java/src/test/java/cn/by1e/h2o/study/java/test/MyTest01.java
@@ -1,7 +1,12 @@
 package cn.by1e.h2o.study.java.test;
 
+import cn.by1e.h2o.study.java.collection.MyHashMap;
 import cn.by1e.ox.core.util.ConsoleUtils;
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author bangquan.qian
@@ -10,21 +15,106 @@ import org.junit.Test;
 public class MyTest01 {
 
     @Test
+    public void test4() {
+        int len = 100_0000;
+
+        Map<Integer, Integer> map1 = new HashMap<>(1, len);
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        StopWatch sw1 = StopWatch.createStarted();
+
+        for (int i = 0; i < len; i++) {
+            map1.put(i, i);
+        }
+        for (int i = 0; i < len; i++) {
+            map1.get(i);
+        }
+
+        sw1.stop();
+
+        StopWatch sw2 = StopWatch.createStarted();
+
+        for (int i = 0; i < len; i++) {
+            map2.put(i, i);
+        }
+        for (int i = 0; i < len; i++) {
+            map2.get(i);
+        }
+
+        sw2.stop();
+
+        ConsoleUtils.sout(sw1.toString());
+        ConsoleUtils.sout(sw2.toString());
+    }
+
+    @Test
+    public void test2() {
+        //    static final int MIN_TREEIFY_CAPACITY = 64;
+        //    static final int TREEIFY_THRESHOLD = 8;
+        Map<Integer, Integer> map = new MyHashMap<>(65, 9);
+        for (int i = 0; i < 10_0000; i++) {
+            map.put(i, i);
+        }
+    }
+
+    @Test
+    public void test3() {
+        int time = 128;
+        int len = 64;
+        int[] num1 = new int[len];
+        int[] num2 = new int[len];
+        for (int i = 0; i < time; i++) {
+            int hash1 = hash1(i);
+            num1[hash1 & len - 1]++;
+
+            int hash2 = hash2(i);
+            num2[hash2 & len - 1]++;
+        }
+        long sum1 = sum(num1);
+        long sum2 = sum(num1);
+        ConsoleUtils.sout(sum1);
+        ConsoleUtils.sout(sum2);
+        ConsoleUtils.sout("done");
+    }
+
+    private long sum(int[] nums) {
+        long sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    private static int hash2(Object key) {
+        int h;
+        return (key == null) ? 0 : key.hashCode();
+    }
+
+    private static int hash1(Object key) {
+        int h;
+        // null为0，扰动函数
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    @Test
     public void test1() {
         int size1 = tableSizeFor(63);
         ConsoleUtils.sout(size1);
 
         int size2 = tableSizeFor(64);
         ConsoleUtils.sout(size2);
+
+        ConsoleUtils.sout("done");
     }
 
     private static final int MAXIMUM_CAPACITY = 1 << 30;
 
     /**
-     * 每一位都置为1，再加1，实现扩大到2^n次
+     * 每一位都置为1，再加1，实现扩大到离cap最近的2^n次
      */
     private static int tableSizeFor(final int cap) {
         String s = Integer.toBinaryString(cap);
+        // 减1是为了64-1=63能最终结果为64，65-1=64最终结果为128
         int n = cap - 1;
         s = Integer.toBinaryString(n);
         n |= n >>> 1;
