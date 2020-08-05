f860d4bd599013b637decd065898c576e7b143f8
bangquan.qian <chainz@foxmail.com>
2020-08-05 14:17:01 +0800

    commit

---
+++ h2o-study-java/src/test/java/cn/by1e/h2o/study/test/java/baisc/MySkipListTest.java
@@ -0,0 +1,16 @@
+package cn.by1e.h2o.study.test.java.baisc;
+
+import cn.by1e.h2o.study.java.collection.MySkipList;
+
+/**
+ * @author bangquan.qian
+ * @date 2020-08-04 16:50
+ */
+public class MySkipListTest {
+
+    public static void main(String[] args) {
+        MySkipList mySkipList = new MySkipList();
+        mySkipList.test();
+    }
+
+}
